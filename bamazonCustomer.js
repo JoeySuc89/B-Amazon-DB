8877659b3c5d955aff6eb2a527f5fbcfc8b38a15
JoeySuc89 <jsucameli1@gmail.com>
2019-05-06 20:26:09 -0400

    basic functionality to show the table to the 'customer' and then prompt them what they want to buy and how much of it

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -10,3 +10,47 @@ var connection = mysql.createConnection({
 });
 
 
+connection.connect(function(err){
+  if (err) throw err;
+  console.log("connected as id " + connection.threadId + "\n");
+  avaialableProducts();
+});
+
+function avaialableProducts(){
+  console.log("Our current inventory is . . . . \n")
+  var query = connection.query(
+    "SELECT * FROM products", function (err,res){
+      for (var i = 0; i < res.length; i++) {
+        console.log(res[i].item_number + " | " + res[i].product_name + " | " + res[i].department_name + " | " +  res[i].price + " | " +  res[i].stock + " | ")
+      }
+      console.log("------------------------------------------------------------");
+    customerPurchase();
+  });
+  console.log("Item-Number" + " | " + "Product Name" + " | " + "Department" + " | " + "Price" + " | " + "Stock" + " | ");
+};
+
+function customerPurchase() {
+  inquirer.prompt([{
+    name: "item_number",
+    type: "input",
+    message: "What item are you looking for? (By Item Number Please): "
+  },
+  {
+    name: "stock",
+    type: "input",
+    message: "How many would you like to purchase?: "
+  }])
+  .then (function(answer){
+    var query = "SELECT item_number, stock FROM products WHERE ? AND ?";
+    connection.query(query, [answer.item_number, answer.stock], function(err, res){
+      for (var i = 0; i < res.length; i++) {
+        console.log("Item_Number: " + res[i].item_number + " | Stock: " + res[i].stock);
+      }
+     avaialableProducts();
+    })
+  })
+};
+
+function remainingStock(){
+  
+}
